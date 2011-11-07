6c84efd6f789c86a1ef5fc4b5a7553dfae58fde5
danlatimer <danlatimer@39c62d8c-5a28-45bf-b041-514f7735fdd6>
2011-11-07 15:52:58 +0000

    updated parser and lexer
    git-svn-id: http://subversion.assembla.com/svn/dposl@10 39c62d8c-5a28-45bf-b041-514f7735fdd6

--- src/main/java/org/ruleml/oojdrew/parsing/generated/POSLParserTokenTypes.java
+++ src/main/java/org/ruleml/oojdrew/parsing/generated/POSLParserTokenTypes.java
@@ -1,63 +1,48 @@
-// $ANTLR 2.7.5 (20050530): "POSLParser-Java.g" -> "POSLParser.java"$
+// $ANTLR 2.7.5 (20050128): "POSLParser-Java.g" -> "POSLParser.java"$
 // OO jDREW - An Object Oriented extension of the Java Deductive Reasoning Engine for the Web
 // Copyright (C) 2005 Marcel Ball
-//
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation; either
 // version 2.1 of the License, or (at your option) any later version.
-//
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
-//
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 package org.ruleml.oojdrew.parsing.generated;
 
-
-/**
- * This Class was generated with ANTLR, you can download the grammar use to create
- * the class from OOjDREW's homepage.
- * <p>Title: OO jDREW</p>
- *
- * <p>Description: Reasoning Engine for the Semantic Web - Supporting OO RuleML
- * 0.88</p>
- *
- * <p>Copyright: Copyright (c) 2005</p>
- *
- * @author Marcel A. Ball
- * @version 0.89
- */
-
+    import jdrew.oo.util.*;
+    import java.util.*;
 
 public interface POSLParserTokenTypes {
-    int EOF = 1;
-    int NULL_TREE_LOOKAHEAD = 3;
-    int IMP = 4;
-    int PERIOD = 5;
-    int COMMA = 6;
-    int LPAREN = 7;
-    int RPAREN = 8;
-    int SEMI = 9;
-    int HAT = 10;
-    int PIPE = 11;
-    int BANG = 12;
-    int LBRACK = 13;
-    int RBRACK = 14;
-    int ARROW = 15;
-    int COLON = 16;
-    int USCORE = 17;
-    int QMARK = 18;
-    int SYMBOL = 19;
-    int QSYMBOL = 20;
-    int URI = 21;
-    int LBRACE = 22;
-    int RBRACE = 23;
-    int COMMENT = 24;
-    int MLCOMMENT = 25;
-    int WS = 26;
+	int EOF = 1;
+	int NULL_TREE_LOOKAHEAD = 3;
+	int IMP = 4;
+	int PERIOD = 5;
+	int COMMA = 6;
+	int LPAREN = 7;
+	int RPAREN = 8;
+	int SEMI = 9;
+	int HAT = 10;
+	int PIPE = 11;
+	int BANG = 12;
+	int LBRACK = 13;
+	int RBRACK = 14;
+	int ARROW = 15;
+	int DHAT = 16;
+	int USCORE = 17;
+	int QMARK = 18;
+	int SYMBOL = 19;
+	int QSYMBOL = 20;
+	int IRI = 21;
+	int COLON = 22;
+	int LBRACE = 23;
+	int RBRACE = 24;
+	int COMMENT = 25;
+	int MLCOMMENT = 26;
+	int WS = 27;
 }
