36df1b78a3775b66a1602ac6b61824c71aee42df
b826t <b826t@unb.ca>
2011-11-19 14:44:45 -0400

    Updated the license text in each source file

--- src/main/java/org/ruleml/oojdrew/Builtins/SinBuiltin.java
+++ src/main/java/org/ruleml/oojdrew/Builtins/SinBuiltin.java
@@ -1,10 +1,19 @@
-// OO jDREW Version 0.89
-// Copyright (c) 2005 Marcel Ball
+// OO jDREW - An Object Oriented extension of the Java Deductive Reasoning Engine for the Web
+// Copyright (C) 2005 Marcel Ball
 //
-// This software is licensed under the LGPL (LESSER GENERAL PUBLIC LICENSE) License.
-// Please see "license.txt" in the root directory of this software package for more details.
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
 //
-// Disclaimer: Please see disclaimer.txt in the root directory of this package.
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 package org.ruleml.oojdrew.Builtins;
 
--- src/main/java/org/ruleml/oojdrew/parsing/TypeQueryParserRuleML.java
+++ src/main/java/org/ruleml/oojdrew/parsing/TypeQueryParserRuleML.java
@@ -1,15 +1,20 @@
-/**
-* Class used to Parse a Type query in RuleML format
-* <p>Title: OO jDREW</p>
-*
-* <p>Description: Reasoning Engine for the Semantic Web - Supporting OO RuleML
-* 0.91</p>
-*
-* <p>Copyright: Copyright (c) 2008</p>
-*
-* @author Ben Craig
-* @version 0.96
-*/
+// OO jDREW - An Object Oriented extension of the Java Deductive Reasoning Engine for the Web
+// Copyright (C) 2008 Ben Craig
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 package org.ruleml.oojdrew.parsing;
 
 import java.io.IOException;
